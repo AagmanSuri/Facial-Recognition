51e3b39c9212c83c119de70cd6d92bed6dc22c44
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-05-30 15:34:50 +0530

    Logo and Navigation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <Logo/>
+    </div>
+  );
+}
+
+export default App;
