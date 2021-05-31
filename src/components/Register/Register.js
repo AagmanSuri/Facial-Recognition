8174ef34487d4da96383cbb2d5bb7507a80d3592
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-05-31 22:58:10 +0530

    div

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,19 +3,19 @@ const Register =({onRouteChange})=>{
     return (
         <article className="mw5 center  br3 pa3 pa4-ns mv3 mw6  shadow-5 ba b--black-10"> 
         <main className="pa4 black-80">
-            <form className="measure">
+            <div className="measure">
                 <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                     <legend className="f1 fw6 ph0 mh0">Register</legend>
                     <div className="mt3">
-                    <label className="db fw6 lh-copy f6" for="name">Name</label>
+                    <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
                     <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="email-address"/>
                     </div>
                     <div className="mt3">
-                    <label className="db fw6 lh-copy f6" for="email-address">Email</label>
+                    <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                     <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
                     </div>
                     <div className="mv3">
-                    <label className="db fw6 lh-copy f6" for="password">Password</label>
+                    <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                     <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
                     </div>
                 </fieldset>
@@ -25,7 +25,7 @@ const Register =({onRouteChange})=>{
                 <div className="lh-copy mt3">
         
                 </div>
-            </form>
+            </div>
         </main>
         </article>
     );
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -3,15 +3,15 @@ const Signin =({onRouteChange})=>{
     return (
         <article className="mw5 center  br3 pa3 pa4-ns mv3 mw6  shadow-5 ba b--black-10"> 
         <main className="pa4 black-80">
-            <form className="measure">
+            <div className="measure">
                 <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                     <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                     <div className="mt3">
-                    <label className="db fw6 lh-copy f6" for="email-address">Email</label>
+                    <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                     <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
                     </div>
                     <div className="mv3">
-                    <label className="db fw6 lh-copy f6" for="password">Password</label>
+                    <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                     <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
                     </div>
                 </fieldset>
@@ -21,7 +21,7 @@ const Signin =({onRouteChange})=>{
                 <div className="lh-copy mt3">
                     <p onClick={()=>onRouteChange('Register')} className="f6 link dim black db pointer">Register</p>
                 </div>
-            </form>
+            </div>
         </main>
         </article>
     );
